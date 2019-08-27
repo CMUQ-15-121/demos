5a6ee9bc57559a40d4b30667923a7ecc2123224f
Ryan Riley <rriley.cs@gmail.com>
2019-08-27 10:31:04 +0300

    Adding lec1 demo code

---
+++ 2019-08-25/MySecondJavaProgram.java
@@ -0,0 +1,17 @@
+public class MySecondJavaProgram {
+
+	/* The program below as a strange bug due to the
+	 * spacing.  Make sure you understand what it is...
+	 */
+	public static void main(String[] args) {
+		int i = 6;
+
+		if (i > 5) {
+			System.out.println("Here is number 1");
+		} else
+			System.out.println("Here is number 2");
+			System.out.println("Here is number 3");
+
+	}
+
+}
---
+++ 2019-08-25/lec1.py
@@ -0,0 +1,5 @@
+i = 0
+print("Hello World")
+i = "Monkey"
+if i > 5:
+    print("Hi")
