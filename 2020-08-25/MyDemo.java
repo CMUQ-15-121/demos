eea996e07e85133f30e6da265cb6413f103e3a67
Ryan Riley <rriley.cs@gmail.com>
2020-08-25 15:02:15 +0300

    Adding lecture 2

---
+++ 2020-08-25/MyDemo.java
@@ -0,0 +1,38 @@
+
+public class MyDemo {
+
+	public static void main(String[] args) {
+		/*
+		int a = 3;
+		int b = 2;
+		double c = 2.0;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		
+		System.out.println(a&b);
+		System.out.println(a|b);
+		
+		System.out.println(3/2);
+		
+		System.out.println(a / c);
+		
+		System.out.println(c / a);
+		
+		System.out.println(c / b);
+		*/
+		
+		int d = 10;
+		/*
+		System.out.println(d);
+		d++; // d = d + 1 OR d += 1
+		System.out.println(d);
+		++d;
+		System.out.println(d);
+		*/
+		System.out.println(d++ + d-- + --d + d++);
+		System.out.println(d);
+		
+	}
+
+}
