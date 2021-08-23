12281252797429cb99decff018dfd14429ea745d
Ryan Riley <rriley.cs@gmail.com>
2021-08-23 14:40:24 +0300

    Lec 1 demo code

---
+++ week1/MyDemo.java
@@ -0,0 +1,19 @@
+
+public class MyDemo {
+	
+	public static void myFunc() {
+		System.out.println("Here is my function");
+	}
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		
+		if (a == 5) {
+			System.out.println("Hi there");
+		} else {
+			System.out.println("Wrong Number");
+		}
+		myFunc();
+	}
+}
---
+++ week1/lec1.py
@@ -0,0 +1,11 @@
+a = 5
+
+if a == 5:
+    print("Hi there!")
+    print("Hey Bob")
+else:
+    print("Wrong number!")
+    
+print("Hi")
+      
+      
