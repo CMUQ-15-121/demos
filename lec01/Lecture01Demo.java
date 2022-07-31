8079c90104654ca00b9c2193877fe51b92b958d4
Ryan Riley <rriley.cs@gmail.com>
2022-07-31 15:46:47 +0300

    Lec01 demos

---
+++ lec01/Lecture01Demo.java
@@ -0,0 +1,23 @@
+// Single line comment
+
+/* Multi line comment
+ * Here you go
+ * More
+ */
+
+/** Java doc comment **/
+
+public class Lecture01Demo {
+	public static void main(String[] args) {
+		int a = 5;
+
+		if (a == 5) {
+			System.out.println("Hi there!");
+		} else {
+			System.out.println("Wrong Number!");
+			System.out.println("Hey Bob");
+		}
+
+		System.out.println("Hi");
+	}
+}
---
+++ lec01/sample.py
@@ -0,0 +1,23 @@
+# This is a comment
+
+# Line 1
+# Line 2
+# Line 3
+# Line 4
+
+"""
+Line 1
+Line 2
+Line 3
+Line 4
+"""
+
+a = 5
+
+if a == 5:
+    print("Hi there!")
+else:
+    print("Wrong number!")
+    print("Hey Bob")
+    
+print("Hi")
